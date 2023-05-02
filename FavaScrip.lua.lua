2b4515f4b048ecb5cc300669bdc0257423d09273
FavaScripts <121361935+FavaScripts@users.noreply.github.com>
2023-05-02 22:23:55 +0200

    Add files via upload

---
+++ FavaScrip.lua.lua
@@ -0,0 +1,16 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("PSX Trade Scam/Dupe Hub", "BloodTheme")
+
+-- MAIN
+local Tab = Window:NewTab("Main")
+local MainSection = Tab:NewSection("Main")
+
+workbench.trust.manag
+MainSection:NewButton("Trade Scam", "Before accepting a trade click this", function()
+end)  
+    
+
+
+
+MainSection:NewButton("Dupe pets", "With a bank depoit your pets, click this, it will make rejoin you then you pets are duped", function()
+end)
